839002fa982adf8a9405ed0f6fcdebe6d9c527fb
Данијел Митровић <rodjenihm@outlook.com>
2020-05-08 19:24:49 +0200

    Add Carts table

---
+++ ZrakPizza/ZrakPizza.Database/dbo/Tables/Carts.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Carts]
+(
+	[Id]					NVARCHAR(32)		NOT NULL,
+	[ItemsCount]			INT					NOT NULL DEFAULT 0,
+	[ItemsTotalPrice]		MONEY				NOT NULL DEFAULT 0
+	CONSTRAINT [PK_Carts] PRIMARY KEY ([Id])
+)
