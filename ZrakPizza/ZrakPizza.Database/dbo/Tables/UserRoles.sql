b076037185936c23e93f8160b558edcbff7cac74
Данијел Митровић <rodjenihm@outlook.com>
2020-05-08 19:13:53 +0200

    Add UserRoles table

---
+++ ZrakPizza/ZrakPizza.Database/dbo/Tables/UserRoles.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserRoles]
+(
+	[UserId] NVARCHAR(32) NOT NULL,
+	[RoleId] NVARCHAR(32) NOT NULL,
+	CONSTRAINT [PK_UserRoles] PRIMARY KEY ([UserId], [RoleId]),
+    CONSTRAINT [FK_UserRoles_Users_Id] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE CASCADE,
+	CONSTRAINT [FK_UserRoles_Roles_Id] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[Roles] ([Id]) ON DELETE CASCADE
+)
