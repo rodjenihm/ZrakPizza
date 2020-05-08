d627d1ab09d8fe11e383c410495b712bdf831f15
Данијел Митровић <rodjenihm@outlook.com>
2020-05-08 19:04:18 +0200

    Add ProductCategories table

---
+++ ZrakPizza/ZrakPizza.Database/dbo/Tables/ProductCategories.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ProductCategories]
+(
+	[ProductId]							NVARCHAR(32) NOT NULL,
+	[CategoryId]						NVARCHAR(32) NOT NULL,
+	CONSTRAINT [PK_ProductCategories] PRIMARY KEY ([ProductId], [CategoryId]),
+	CONSTRAINT [FK_ProductCategories_Products_Id] FOREIGN KEY ([ProductId]) REFERENCES [dbo].[Products] ([Id]) ON DELETE CASCADE,
+	CONSTRAINT [FK_ProductCategories_Categories_Id] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Categories] ([Id]) ON DELETE CASCADE
+)
