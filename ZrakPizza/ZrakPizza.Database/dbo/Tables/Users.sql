7b7ad2d5c85dee0e5e9ebdaaa7f7f06470d1343d
Данијел Митровић <rodjenihm@outlook.com>
2020-05-08 17:21:24 +0200

    Add Users table

---
+++ ZrakPizza/ZrakPizza.Database/dbo/Tables/Users.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id]					NVARCHAR(32)	NOT NULL,
+	[UserName]				NVARCHAR(255)	NOT NULL,
+	[Name]					NVARCHAR(255)			,
+	[PasswordHash]			NVARCHAR(48)	NOT NULL,
+	CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+)
+
+GO
+
+CREATE UNIQUE INDEX [IX_Users_UserName] ON [dbo].[Users] ([UserName])
