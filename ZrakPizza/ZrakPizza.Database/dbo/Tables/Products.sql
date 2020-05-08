0705e44e256a0b9359025daaa0b4cada980eec6f
Данијел Митровић <rodjenihm@outlook.com>
2020-05-08 18:46:11 +0200

    Add Products table

---
+++ ZrakPizza/ZrakPizza.Database/dbo/Tables/Products.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[Id]						NVARCHAR(32)	NOT NULL,
+	[Name]						NVARCHAR(MAX)	NOT NULL,
+	[Description]				NVARCHAR(MAX)	NOT NULL,
+	[ImageUrl]					NVARCHAR(MAX)	NOT NULL,
+	CONSTRAINT [PK_Products] PRIMARY KEY ([Id])
+)
