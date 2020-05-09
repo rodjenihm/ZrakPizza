5afcd145ca79853dfe5ba3c62835ff9605f54852
Данијел Митровић <rodjenihm@outlook.com>
2020-05-09 19:56:44 +0200

    Add UserProfile automapper configuration

---
+++ ZrakPizza/ZrakPizza.Web/Profiles/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZrakPizza.DataAccess.Entities;
+using ZrakPizza.Web.Dto;
+using ZrakPizza.Web.Resources;
+
+namespace ZrakPizza.Web.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserDto, User>();
+            CreateMap<User, UserResource>();
+        }
+    }
+}
