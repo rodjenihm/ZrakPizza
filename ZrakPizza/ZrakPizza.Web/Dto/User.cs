e5e99508a237ceb9a557788b38bc766587317a3f
Данијел Митровић <rodjenihm@outlook.com>
2020-05-09 19:30:34 +0200

    Add User data transfer object

---
+++ ZrakPizza/ZrakPizza.Web/Dto/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZrakPizza.Web.Dto
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
