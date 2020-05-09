96fc575816b6bf72d79efe04baaeeebc200c3426
Данијел Митровић <rodjenihm@outlook.com>
2020-05-09 19:15:42 +0200

    Add ConnectionString wrapper for connection string (for IoC)

---
+++ ZrakPizza/ZrakPizza.DataAccess/ConnectionString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZrakPizza.DataAccess
+{
+    public class ConnectionString
+    {
+        public string Value { get; set; }
+    }
+}
