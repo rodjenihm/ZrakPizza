97eb216a480afbad9480d167cf96f71a71603dda
Данијел Митровић <rodjenihm@outlook.com>
2020-05-09 23:51:01 +0200

    Add Carts_Create stored procedure

---
+++ ZrakPizza/ZrakPizza.Database/dbo/Stored Procedures/Carts_Create.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[Carts_Create]
+	@Id NVARCHAR(32),
+	@ItemsCount INT,
+	@ItemsTotalPrice MONEY
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	INSERT INTO [dbo].[Carts]
+	([Id], [ItemsCount], [ItemsTotalPrice])
+	VALUES
+	(@Id, @ItemsCount, @ItemsTotalPrice)
+END
