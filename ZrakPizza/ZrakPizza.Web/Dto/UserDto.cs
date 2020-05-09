f8eaf2248804975ed29affb2207744467999adcd
Данијел Митровић <rodjenihm@outlook.com>
2020-05-09 20:09:02 +0200

    Add Data anotations

--- ZrakPizza/ZrakPizza.Web/Dto/UserDto.cs
+++ ZrakPizza/ZrakPizza.Web/Dto/UserDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,13 @@ namespace ZrakPizza.Web.Dto
 {
     public class UserDto
     {
+        [Required]
+        [StringLength(255, MinimumLength = 4)]
         public string UserName { get; set; }
+        [StringLength(255)]
         public string Name { get; set; }
+        [Required]
+        [MinLength(6)]
         public string Password { get; set; }
     }
 }
