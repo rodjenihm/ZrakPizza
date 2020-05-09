bc4adf292db98d9d30cae3ccc4809d3ec27abda9
Данијел Митровић <rodjenihm@outlook.com>
2020-05-09 21:31:54 +0200

    Add Products_GetAll and Products_GetAllWithVariants stored procedures

---
+++ ZrakPizza/ZrakPizza.Database/dbo/Stored Procedures/Products_GetAll.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[Products_GetAll]
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT
+    p.*, c.Name as Category
+    FROM [dbo].[Products] p
+    INNER JOIN [dbo].[ProductCategories] pc ON pc.ProductId = p.Id
+    INNER JOIN [dbo].[Categories] c ON c.Id = pc.CategoryId
+END
---
+++ ZrakPizza/ZrakPizza.Database/dbo/Stored Procedures/Products_GetAllWithVariants.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[Products_GetAllWithVariants]
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT
+    p.*, c.Name as Category, pv.*
+    FROM [dbo].[Products] p
+    INNER JOIN [dbo].[ProductCategories] pc ON pc.ProductId = p.Id
+    INNER JOIN [dbo].[Categories] c ON c.Id = pc.CategoryId
+    INNER JOIN [dbo].[ProductVariants] pv ON pv.ProductId = p.Id
+END
