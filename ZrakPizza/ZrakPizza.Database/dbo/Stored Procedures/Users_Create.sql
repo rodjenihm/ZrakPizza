9b45787dcfd6bf29120d35ac3e6aa6ab89940f1f
Данијел Митровић <rodjenihm@outlook.com>
2020-05-09 20:37:11 +0200

    Add Users_Create stored procedure

---
+++ ZrakPizza/ZrakPizza.Database/dbo/Stored Procedures/Users_Create.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[Users_Create]
+	@Id NVARCHAR(32),
+	@UserName NVARCHAR(255),
+	@Name NVARCHAR(255),
+	@PasswordHash NVARCHAR(48)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	INSERT INTO [dbo].[Users]
+	([Id], [UserName], [Name], [PasswordHash])
+	VALUES
+	(@Id, @UserName, @Name, @PasswordHash)
+END
