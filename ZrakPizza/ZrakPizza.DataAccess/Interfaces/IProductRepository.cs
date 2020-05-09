4ff77f2ea646ee2176d2632a66199fed9f18b3b6
Данијел Митровић <rodjenihm@outlook.com>
2020-05-09 21:32:32 +0200

    Add ProductRepository implementation

---
+++ ZrakPizza/ZrakPizza.DataAccess/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ZrakPizza.DataAccess.Entities;
+
+namespace ZrakPizza.DataAccess
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAll(bool includeVariants);
+    }
+}
---
+++ ZrakPizza/ZrakPizza.DataAccess/ProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using ZrakPizza.DataAccess.Entities;
+
+namespace ZrakPizza.DataAccess
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ConnectionString _connectionString;
+
+        public ProductRepository(ConnectionString connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IEnumerable<Product>> GetAll(bool includeVariants = true)
+        {
+            using (var connection = new SqlConnection(_connectionString.Value))
+            {
+                if (includeVariants)
+                {
+                    var sql = "[Products_GetAllWithVariants]";
+
+                    var dic = new Dictionary<string, Product>();
+
+                    var result = await connection.QueryAsync<Product, Variant, Product>(sql, (p, v) =>
+                    {
+                        if (!dic.TryGetValue(p.Id, out Product product)) dic.Add(p.Id, product = p);
+
+                        product.Variants.Add(v);
+
+                        return p;
+                    });
+
+                    return dic.Values;
+                }
+                else
+                {
+                    var sql = "[Products_GetAll]";
+
+                    var result = await connection.QueryAsync<Product>(sql);
+
+                    return result;
+                }
+            }
+        }
+    }
+}
