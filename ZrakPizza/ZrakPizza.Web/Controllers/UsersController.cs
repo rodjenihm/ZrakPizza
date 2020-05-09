d58959b4c5cba5b4e7a8dad9e68b6b885a835334
Данијел Митровић <rodjenihm@outlook.com>
2020-05-09 19:38:41 +0200

    Add ZrakPizza.Services project

--- ZrakPizza/ZrakPizza.Web/Controllers/UsersController.cs
+++ ZrakPizza/ZrakPizza.Web/Controllers/UsersController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ZrakPizza.DataAccess;
+using ZrakPizza.DataAccess.Entities;
 
 namespace ZrakPizza.Web.Controllers
 {
@@ -11,5 +13,31 @@ namespace ZrakPizza.Web.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly IUserRepository _userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Dto.User user)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var userId = Guid.NewGuid().ToString("N");
+
+            User newUser = new User
+            {
+                Id = userId,
+                UserName = user.UserName,
+                Name = user.Name,
+                PasswordHash = user.Password
+            };
+
+            await _userRepository.Create(newUser);
+
+            return Created($"api/users/{userId}", user);
+        }
     }
 }
