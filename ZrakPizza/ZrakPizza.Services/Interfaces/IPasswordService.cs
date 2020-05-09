930ecdcf29101b44e6ca08d9b378a29f07783b78
Данијел Митровић <rodjenihm@outlook.com>
2020-05-09 19:40:54 +0200

    Add PasswordService implementation

---
+++ ZrakPizza/ZrakPizza.Services/Interfaces/IPasswordService.cs
@@ -0,0 +1,8 @@
+﻿namespace ZrakPizza.Services
+{
+    public interface IPasswordService
+    {
+        string HashPassword(string password);
+        bool VerifyHashedPassword(string providedPassword, string hashedPassword);
+    }
+}
