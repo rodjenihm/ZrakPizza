4fd8aa933dd52f6b7e72395b1ab3eaa35c9e6932
Данијел Митровић <rodjenihm@outlook.com>
2020-05-09 19:47:22 +0200

    Add UserResource

---
+++ ZrakPizza/ZrakPizza.Web/Resources/UserResource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZrakPizza.Web.Resources
+{
+    public class UserResource
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+    }
+}
