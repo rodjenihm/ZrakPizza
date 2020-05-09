3a182db0f5a0345cb106b131531978dd840ad49b
Данијел Митровић <rodjenihm@outlook.com>
2020-05-09 23:29:17 +0200

    Add Cart entity

---
+++ ZrakPizza/ZrakPizza.DataAccess/Entities/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZrakPizza.DataAccess.Entities
+{
+    public class Cart
+    {
+        public string Id { get; set; }
+        public int ItemsCount { get; set; }
+        public decimal ItemsTotalPrice { get; set; }
+
+        public ICollection<Product> Items { get; set; } = new List<Product>();
+    }
+}
