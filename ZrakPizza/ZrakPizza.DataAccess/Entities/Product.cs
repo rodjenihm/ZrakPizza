63abc7652c147efa88e3fbe6c4b4bb82aa79a9eb
Данијел Митровић <rodjenihm@outlook.com>
2020-05-09 21:32:10 +0200

    Add Product and Variant entities

---
+++ ZrakPizza/ZrakPizza.DataAccess/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZrakPizza.DataAccess.Entities
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Category { get; set; }
+
+
+        public ICollection<Variant> Variants { get; set; } = new List<Variant>();
+    }
+}
---
+++ ZrakPizza/ZrakPizza.DataAccess/Entities/Variant.cs
@@ -0,0 +1,9 @@
+﻿namespace ZrakPizza.DataAccess.Entities
+{
+    public class Variant
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
