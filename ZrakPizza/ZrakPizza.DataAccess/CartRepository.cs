7cc00923a38229eb8bad3158e218755146c78857
Данијел Митровић <rodjenihm@outlook.com>
2020-05-09 23:55:05 +0200

    Add CartRepository implementation

---
+++ ZrakPizza/ZrakPizza.DataAccess/CartRepository.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using ZrakPizza.DataAccess.Entities;
+
+namespace ZrakPizza.DataAccess
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly ConnectionString _connectionString;
+
+        public CartRepository(ConnectionString connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task Create(Cart cart)
+        {
+            using (var connection = new SqlConnection(_connectionString.Value))
+            {
+                var sql = "[Carts_Create] @Id, @ItemsCount, @ItemsTotalPrice";
+
+                var result = await connection.ExecuteAsync(sql, cart);
+            }
+        }
+    }
+}
