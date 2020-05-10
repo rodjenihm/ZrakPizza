8ef287c2d3839114bcba7406d99eea1e312e80d8
Данијел Митровић <rodjenihm@outlook.com>
2020-05-10 14:54:29 +0200

    Add AuthenticateController

---
+++ ZrakPizza/ZrakPizza.Web/Controllers/AuthenticateController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZrakPizza.Services;
+using ZrakPizza.Web.Dto;
+
+namespace ZrakPizza.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly IAuthenticateService _authenticateService;
+
+        public AuthenticateController(IAuthenticateService authenticateService)
+        {
+            _authenticateService = authenticateService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(UserCredentialsDto userCredentialsDto)
+        {
+            var token = await _authenticateService.GenerateToken(userCredentialsDto.Username, userCredentialsDto.Password);
+
+            if (token == null)
+                return Ok("Username and/or password are incorrect");
+
+            return Ok(new { token = token });
+        }
+    }
+}
