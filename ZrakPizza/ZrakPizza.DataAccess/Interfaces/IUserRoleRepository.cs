dd49868a7cade8dc6e0053af7b0b4753038f2da4
Данијел Митровић <rodjenihm@outlook.com>
2020-05-10 13:24:51 +0200

    Add UserRoleRepository implementation

---
+++ ZrakPizza/ZrakPizza.DataAccess/Interfaces/IUserRoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ZrakPizza.DataAccess.Entities;
+
+namespace ZrakPizza.DataAccess
+{
+    public interface IUserRoleRepository
+    {
+        Task<IEnumerable<Role>> GetRolesForUser(string userId);
+    }
+}
---
+++ ZrakPizza/ZrakPizza.DataAccess/UserRoleRepository.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using ZrakPizza.DataAccess.Entities;
+
+namespace ZrakPizza.DataAccess
+{
+    public class UserRoleRepository : IUserRoleRepository
+    {
+        private readonly ConnectionString _connectionString;
+
+        public UserRoleRepository(ConnectionString connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IEnumerable<Role>> GetRolesForUser(string userId)
+        {
+            using (var connection = new SqlConnection(_connectionString.Value))
+            {
+                var sql = "[UserRoles_GetRolesForUser] @UserId";
+
+                var result = await connection.QueryAsync<Role>(sql, new { UserId = userId });
+
+                return result;
+            }
+        }
+    }
+}
