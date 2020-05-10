93ca30c49d2e8b5a2c65efa93fc070c634431c31
Данијел Митровић <rodjenihm@outlook.com>
2020-05-10 11:31:06 +0200

    Add Users_UserCreated trigger

---
+++ ZrakPizza/ZrakPizza.Database/dbo/Triggers/Users_UserCreated.sql
@@ -0,0 +1,14 @@
+﻿CREATE TRIGGER Users_UserCreated ON [dbo].[Users]
+FOR INSERT 
+AS
+BEGIN
+	
+	DECLARE @memberId NVARCHAR(32) = (
+		SELECT [Id] FROM [dbo].[Roles] WHERE [Name] = 'member'
+	)
+
+	INSERT INTO [dbo].[UserRoles]
+	([UserId], [RoleId])
+	SELECT Id , @memberId
+	FROM INSERTED
+END
