fea637cb21e3d7e8c67f7901c0f3a3c6a3c0c80b
Данијел Митровић <rodjenihm@outlook.com>
2020-05-10 13:24:23 +0200

    Add UserRoles_GetRolesForUser stored procedure

---
+++ ZrakPizza/ZrakPizza.Database/dbo/Stored Procedures/UserRoles_GetRolesForUser.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[UserRoles_GetRolesForUser]
+	@UserId INT
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT
+	r.[Id], r.[Name]
+	FROM [dbo].[UserRoles] u
+	INNER JOIN [dbo].[Roles] r ON r.Id = u.RoleId 
+	WHERE [UserId] = @UserId
+END
