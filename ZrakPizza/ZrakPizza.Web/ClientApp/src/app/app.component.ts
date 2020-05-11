f6015f55be48a8dce0de9d07f535115e0ecaf77d
Данијел Митровић <rodjenihm@outlook.com>
2020-05-11 04:06:23 +0200

    Initial home page design

--- ZrakPizza/ZrakPizza.Web/ClientApp/src/app/app.component.ts
+++ ZrakPizza/ZrakPizza.Web/ClientApp/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ClientApp';
+export class AppComponent implements OnInit {
+  products$;
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.products$ = this.productService.getAll(true);
+  }
 }
--- ZrakPizza/ZrakPizza.Web/ClientApp/src/app/app.module.ts
+++ ZrakPizza/ZrakPizza.Web/ClientApp/src/app/app.module.ts
@@ -4,17 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductService } from './services/product.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductComponent } from './product/product.component';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
