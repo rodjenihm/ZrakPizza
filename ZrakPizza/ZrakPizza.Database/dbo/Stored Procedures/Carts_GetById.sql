14046e4881f4e37010c042d281c0286fc7262c34
Данијел Митровић <rodjenihm@outlook.com>
2020-05-11 19:03:16 +0200

    Add Carts_GetById stored procedure

---
+++ ZrakPizza/ZrakPizza.Database/dbo/Stored Procedures/Carts_GetById.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[Carts_GetById]
+	@Id NVARCHAR(32)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT
+	c.[Id], c.[ItemsCount], c.[ItemsTotalPrice]
+	FROM [dbo].[Carts] c
+	WHERE [Id] = @Id
+END
