cf3ae67f8eb92f46819e7b35b184316419aed868
Данијел Митровић <rodjenihm@outlook.com>
2020-05-13 00:10:43 +0200

    Add product variant in cart counter

--- ZrakPizza/ZrakPizza.Web/ClientApp/src/app/product/product.component.ts
+++ ZrakPizza/ZrakPizza.Web/ClientApp/src/app/product/product.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../models/product';
 import { ProductOption } from '../models/product.option';
 import { CartService } from '../services/cart.service';
-import { Cart } from '../models/cart';
 import { ProductVariant } from '../models/product.variant';
 
 @Component({
@@ -15,14 +14,12 @@ export class ProductComponent implements OnInit {
 
   selectedOption: ProductOption;
 
-  cart: Cart;
-
   constructor(private cartService: CartService) {
   }
 
   ngOnInit() {
     this.selectedOption = this.product.productOptions[0];
-    this.cart = this.cartService.getCart();
+    this.cartService.getCart().items.reduce((acc, cur) => cur.id === this.selectedOption.id ? ++acc : acc, 0)
   }
 
   addToCart() {
@@ -32,10 +29,28 @@ export class ProductComponent implements OnInit {
       description: this.product.description,
       category: this.product.category,
       imageUrl: this.product.imageUrl,
-      variantdescription: this.selectedOption.description,
+      variantDescription: this.selectedOption.description,
       price: this.selectedOption.price
     };
 
     this.cartService.addToCart(productVariant);
+  }
+
+  removeFromCart() {
+    const productVariant: ProductVariant = {
+      id: this.selectedOption.id,
+      name: this.product.name,
+      description: this.product.description,
+      category: this.product.category,
+      imageUrl: this.product.imageUrl,
+      variantDescription: this.selectedOption.description,
+      price: this.selectedOption.price
+    };
+
+    this.cartService.removeFromCart(productVariant);
+  }
+
+  selectedOptionInCartCount() {
+    return this.cartService.getCart().items.reduce((acc, cur) => cur.id === this.selectedOption.id ? ++acc : acc, 0);
   }
 }
