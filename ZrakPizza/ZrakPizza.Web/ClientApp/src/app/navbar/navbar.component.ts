6df377476bba9817c7b3f372b4633221d387020a
Данијел Митровић <rodjenihm@outlook.com>
2020-05-14 02:55:32 +0200

    Improve navbar: show login and register only if user is not authenticated, show logout if user is authenticated

--- ZrakPizza/ZrakPizza.Web/ClientApp/src/app/navbar/navbar.component.ts
+++ ZrakPizza/ZrakPizza.Web/ClientApp/src/app/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart.service';
 import { Cart } from '../models/cart';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -10,14 +12,24 @@ import { Cart } from '../models/cart';
 export class NavbarComponent implements OnInit {
   cart: Cart;
 
+  authService: AuthService;
   cartService: CartService;
 
-  constructor(cartService: CartService) {
+  constructor(
+    authService: AuthService,
+    cartService: CartService,
+    private router: Router) {
+    this.authService = authService;
     this.cartService = cartService;
   }
 
   ngOnInit() {
     this.cart = this.cartService.getCart();
+  }
+
+  logout() {
+    this.authService.deauthenticate();
+    this.router.navigate(['/']);
   }
 
 }
