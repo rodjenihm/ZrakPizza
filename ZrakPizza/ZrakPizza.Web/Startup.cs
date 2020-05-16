426eadea80c93d264d5182437dbc4d0fb5f0abbd
Данијел Митровић <rodjenihm@outlook.com>
2020-05-16 02:47:34 +0200

    Separate Backend and ClientApp (deployment issues)

--- ZrakPizza/ZrakPizza.Web/Startup.cs
+++ ZrakPizza/ZrakPizza.Web/Startup.cs
@@ -44,11 +44,6 @@ namespace ZrakPizza.Web
             services.AddAutoMapper(typeof(Startup));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/dist";
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,28 +60,12 @@ namespace ZrakPizza.Web
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseSpaStaticFiles();
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller}/{action=Index}/{id?}");
-            });
-
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "ClientApp";
-
-                spa.Options.StartupTimeout = new TimeSpan(0, 5, 0);
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
             });
         }
     }
